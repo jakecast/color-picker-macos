64f40212cfa3cfe86012167d065c56f29c995806
Jake Kirshner <jake.kirshner@gmail.com>
2016-07-07 09:14:56 -0400

    updated against dev id signed app

--- color-picker/AppDelegate.swift
+++ color-picker/AppDelegate.swift
@@ -1,19 +1,17 @@
 import Cocoa
 
+
 @NSApplicationMain
 internal final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     internal var colorPanel: NSColorPanel {
         return NSColorPanel.shared()
     }
-
     internal var sharedApplication: NSApplication {
         return NSApplication.shared()
     }
-
     internal var userDefaults: UserDefaults {
         return UserDefaults.standard
     }
-
     internal var userDefaultsController: NSUserDefaultsController {
         return NSUserDefaultsController.shared()
     }
@@ -26,7 +24,6 @@ internal final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDeleg
         self.colorPanel.hidesOnDeactivate = false
         self.colorPanel.showsAlpha = true
         self.colorPanel.title = "Color Panel"
-
         self.colorPanel.makeKeyAndOrderFront(nil)
     }
 
